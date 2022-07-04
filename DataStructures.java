851a43e15c4d256cf09c85b5b35347ac7553aa12
mdafrozz <106298595+mdafrozz@users.noreply.github.com>
2022-07-04 09:17:50 +0530

    Refactor: Ability to insert 30 between 56  and 70

--- DataStructures.java
+++ DataStructures.java
@@ -14,8 +14,9 @@ public class DataStructures {
 		
 		Operations myLinkedList = new Operations();
 		Operations myLinkedList1 = new Operations();
+		Operations myLinkedList2 = new Operations();
+		
 		System.out.println("<------------Add node in start----------->");
-
 		myLinkedList.addNode(70);
 		myLinkedList.addNode(30);
 		myLinkedList.addNode(56);
@@ -25,6 +26,12 @@ public class DataStructures {
 		myLinkedList1.append(56);
 		myLinkedList1.append(30);
 		myLinkedList1.append(70);
-		myLinkedList.PrintList();
+		myLinkedList1.PrintList();
+		
+		System.out.println("<------------Insert between two nodes----------->");
+		myLinkedList2.insert(56);
+		myLinkedList2.insert(70);
+		myLinkedList2.insertBetweenNode(56, 70, 30);
+		myLinkedList2.PrintList();
 	}
 }
